341e9143edcda0732e8c332ce2ad76d27be5d3d1
ramon79 <47372895+ramon79@users.noreply.github.com>
2019-03-24 23:26:37 +0900

    part4_script 만들기

---
+++ CoinGame/Assets/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    int count = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update" + count);
+        count++;
+        
+    }
+}
