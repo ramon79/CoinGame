e33ede188f8931bc1660bf974befbbeaef6fb1e0
ramon79 <47372895+ramon79@users.noreply.github.com>
2019-03-30 23:40:38 +0900

    part5 finished

--- CoinGame/Assets/Scripts/Ball.cs
+++ CoinGame/Assets/Scripts/Ball.cs
@@ -5,18 +5,65 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     int count = 1;
+    float startingPoint;
+    // SphereCollider myCollider;
+
+    bool shouldPrintOver20 = true;
+    bool shouldPrintOver30 = true;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start");
+        // myCollider = GetComponent<SphereCollider>();
+        // Rigidbody myRigidbody = GetComponent<Rigidbody>();
+        // Debug.Log("Use Gravity" + myRigidbody.useGravity);
+        // Debug.Log("Start");
+        // 볼의 처음 시작 위치
+        startingPoint = transform.position.z;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update" + count);
-        count++;
+        // myCollider.radius = myCollider.radius + 0.01f;
+        float distance;
+        distance = transform.position.z - startingPoint;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up*300);
+            // Debug.Log("Space를 눌렀습니다.");
+
+        }
+        // 볼의 위치 받아오기
+        // Debug.Log("이동거리는 = " + distance);
+
+        if(distance > 30)
+        {
+            //Debug.Log("Over 30:" + distance);
+            if(shouldPrintOver30)
+            {
+                Debug.Log("Over 30:" + distance);
+                shouldPrintOver30 = false;
+
+            }
+        }
+        else if(distance >20)
+        {
+            if(shouldPrintOver20)
+            {
+                Debug.Log("Over 20:" + distance);
+                shouldPrintOver20 = false;
+
+            }
+            
+        }
+        else
+        {
+           // Debug.Log("Less than 20:" + distance);
+        }
+        
+ 
         
     }
 }
